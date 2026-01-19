a6bc7b2fe42c07b6e223a3b072ef5960de0d5a3b
ahmad lutfy <ahmadlutfy577@gmail.com>
2026-01-19 21:12:54 +0300

    Change response from 'hi' to 'hello'

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/" ,(req,res) => {
-    res.send("hi");
+    res.send("hello");
 })
 app.listen(3000 , ()=> {
     console.log("server is running");
